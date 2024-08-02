591737e5a882ae093151eb1c39b82d559001d162
Marcus Ziadé <guitaripod@icloud.com>
2024-08-01 19:30:22 -0700

    Add string reverse algo

---
+++ reverse_string.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+class Solution {
+    func reverseString(_ s: inout [Character]) {
+        var left = 0
+        var right = s.count - 1
+
+        while left < right {
+            let temp = s[left]
+            s[left] = s[right]
+            s[right] = temp
+            left += 1
+            right -= 1
+        }
+    }
+}
